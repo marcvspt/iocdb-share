4e12f2d0b8f9a940f8dcce4a8c8d8fd31df6534d
marcvspt <78035694+marcvspt@users.noreply.github.com>
2024-11-28 00:23:09 -0600

    Initial version - not email analyze

---
+++ src/pages/api/type.ts
@@ -0,0 +1,34 @@
+export async function GET({ request }: { request: Request }) {
+    const { url } = await request;
+    const urlObject = new URL(url);
+    const ioc = urlObject.searchParams.get("ioc")
+
+    if (!ioc) {
+        return new Response(JSON.stringify({ error: "Invalid input" }), {
+            status: 400,
+        });
+    }
+    /* if (!result.success) {
+        return new Response(JSON.stringify({ error: "Invalid input" }), {
+            status: 400,
+        });
+    } */
+
+    // Simple regex patterns for each type
+    const patterns = {
+        ip: /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/,
+        domain: /^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/,
+        email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        hash: /^[a-fA-F0-9]{32,64}$/,
+    };
+
+    for (const [type, pattern] of Object.entries(patterns)) {
+        if (pattern.test(ioc)) {
+            return new Response(JSON.stringify({ type }));
+        }
+    }
+
+    return new Response(JSON.stringify({ error: "Unknown IoC type" }), {
+        status: 400,
+    });
+}
---
+++ src/utils/domain.ts
@@ -0,0 +1,36 @@
+const API_VT_DOMAIN = "https://www.virustotal.com/api/v3/domains"
+const API_OTX_DOMAIN = "https://otx.alienvault.com/api/v1/indicators/domain"
+
+export async function analyzeDomain(domain: string) {
+    const virustotalKey = import.meta.env.VIRUSTOTAL_API_KEY;
+    const otxKey = import.meta.env.OTX_API_KEY;
+
+
+    const urlVTDomainInfo = `${API_VT_DOMAIN}/${domain}`
+    const virustotalResponse = await fetch(urlVTDomainInfo, {
+        headers: {
+            'x-apikey': virustotalKey,
+        },
+    });
+
+    const urlOTXDomainInfo = `${API_OTX_DOMAIN}/${domain}/general`
+    const otxResponse = await fetch(urlOTXDomainInfo, {
+        headers: {
+            'X-OTX-API-KEY': otxKey,
+        },
+    });
+
+    const virustotalData = await virustotalResponse.json();
+    const otxData = await otxResponse.json();
+
+    return {
+        virustotal: {
+            malicious: virustotalData.data.attributes.last_analysis_stats.malicious,
+            suspicious: virustotalData.data.attributes.last_analysis_stats.suspicious,
+        },
+        otx: {
+            pulseCount: otxData.pulse_info.count,
+        },
+    };
+}
+
