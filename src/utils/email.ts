e7d9f3325badcf30da1ae411bf55293ce18c60b8
marcvspt <78035694+marcvspt@users.noreply.github.com>
2024-11-28 00:50:06 -0600

    Delete email analyze function (temporal)

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,36 +1,7 @@
-const API_EMAILREP = "https://emailrep.io"
-const API_HIBP = "https://haveibeenpwned.com/api/v3/breachedaccount"
-
 export async function analyzeEmail(email: string) {
-    const emailrepKey = import.meta.env.EMAILREP_API_KEY;
-    const hibpKey = import.meta.env.HIBP_API_KEY;
-
-    const urlEmailrepInfo = `${API_EMAILREP}/${email}`
-    const emailrepResponse = await fetch(urlEmailrepInfo, {
-        headers: {
-            'Key': emailrepKey,
-        },
-    });
-
-    const urlHIBPInfo = `${API_HIBP}/${email}`
-    const hibpResponse = await fetch(urlHIBPInfo, {
-        headers: {
-            'hibp-api-key': hibpKey,
-        },
-    });
-
-    const emailrepData = await emailrepResponse.json();
-    const hibpData = await hibpResponse.json();
-
     return {
-        emailrep: {
-            reputation: emailrepData.reputation,
-            suspicious: emailrepData.suspicious,
-        },
-        haveibeenpwned: hibpData.map(breach => ({
-            name: breach.Name,
-            date: breach.BreachDate,
-        })),
+        emailrep: "Not implemented yet",
+        haveibeenpwned: "Not implemented yet",
     };
 }
 
